7cfdaad9ac2867435a939df0d33af7234e38182a
LucasJC <lucas.couchot@gmail.com>
2025-05-26 18:29:01 -0500

    Inicializar proyecto

---
+++ app.py
@@ -0,0 +1,22 @@
+# Ejecución de parte A
+# dnis = ingresar_dnis()
+dnis = [ "39057467", "37660974", "36899913", "36493716", "34330039" ]
+
+# TODO ingreso de DNIs (Plantear lógica que perimita ingresar DNIs y tenga una opción de fin para continuar)
+# TODO Generación de conjuntos de dígitos únicos 36493716 -> [3, 6, 4, 9, 7, 1]
+# TODO Cálculo y visualización de unión, intersección y diferencias y diferencia simétrica
+# TODO Conteo de frecuencia de cada dígito en cada DNI usando estructuras repetitivas
+# TODO Suma total de dígitos de cada DNI
+# TODO evaluación de condiciones lógicas planteadas en consigna original (Tenemos que definir al menos 2)
+
+
+# Ejecución de parte B
+# años_nacimiento = ingresar_años_nacimiento()
+años_nacimiento = [1990, 1995, 2000, 2005, 2010]
+
+# TODO ingreso de años de nacimiento (Idem carga de dnis, plantear carga por teclado)
+# TODO contar nacidos en años pares e impares
+# TODO si todos nacieron después del 2000, mostrar mensaje "Grupo Z"
+# TODO función que determine si un año es bisiesto
+# TODO si alguno nació en un año bisiesto, mostrar mensaje "Tenemos un año especial"
+# TODO calcular el producto cartesiano entre el conjunto de años y el conjunto de edades actuales
