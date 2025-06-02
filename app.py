3b6ac4ee71997a1ca85137e98fe958e711ae61e1
LucasJC <lucas.couchot@gmail.com>
2025-06-02 18:47:45 -0500

    Limpiamos líneas de más

--- app.py
+++ app.py
@@ -68,45 +68,37 @@ def procesar_operaciones_conjuntos(titulo, funcion, conjuntos, pares_unicos = Tr
                 resultado = funcion(conjuntos[i], conjuntos[j])
                 imprimir_resultado_operacion(titulo, i, j, resultado)
 
-
-#Funcion para contar la frecuencia de cada digito en cada DNI
-# input a utilizar en la funcion
-dnis_completos = [ "39057467", "37660974", "36899913", "36493716", "34330039" ]
-# se define la funcion
+# Función para contar la frecuencia de cada digito en cada DNI
 def contar_frecuencia_digitos(dnis_completos):
     print("Frecuencia de dígitos:")
-# for que recorre cada dni de la lista
+    # for que recorre cada dni de la lista
     for dni in dnis_completos:
-# se inicializa diccionario para almacenar la frecuencia por cada digito del 0 al 9
+        # se inicializa diccionario para almacenar la frecuencia por cada digito del 0 al 9
         frecuencia_digitos = {
             '0': 0, '1': 0, '2': 0, '3': 0, '4': 0,
             '5': 0, '6': 0, '7': 0, '8': 0, '9': 0
         }
-# for que recorre cada digito de los dnis
+        # for que recorre cada digito de los dnis
         for digito in dni:
-# se suma 1 a la frecuencia de cada digito presentado
+            # se suma 1 a la frecuencia de cada digito presentado
             if digito in frecuencia_digitos:
                 frecuencia_digitos[digito] += 1
-# muestra los resultados por cada dni
+        # muestra los resultados por cada dni
         print(f"Para el DNI {dni}, la frecuencia es:")
         for digito, cantidad in frecuencia_digitos.items():
             print(f"Dígito {digito}: {cantidad} veces")
 
-
-#Funcion para los digitos de cada DNI
-# input a utilizar en la funcion        
-dnis_completos = [ "39057467", "37660974", "36899913", "36493716", "34330039" ]
 def sumar_total_digitos(dnis_completos):
-# se define la lista vacia resultado para contener la suma de los digitos de cada dni
+    # se define la lista vacia resultado para contener la suma de los digitos de cada dni
     resultado = []
-# for para recorrer cada sublista en la lista anidada. digitos_dni es la variable iterativa
+    # for para recorrer cada sublista en la lista anidada. digitos_dni es la variable iterativa
     for digitos_dni in dnis_completos:
-# se inicializa la variable suma para guardar el resultado de la suma de los digitos de cada dni
+        # se inicializa la variable suma para guardar el resultado de la suma de los digitos de cada dni
         suma = 0
-# for donde digito es la variable iterativa, recorre cada digito en un dni y acumula la suma de sus valores 
+        # for donde digito es la variable iterativa, recorre cada digito en un dni y acumula la suma de sus valores 
         for digito in digitos_dni:
             suma += int(digito)
-# se agrega el resultado de esta sumatoria al final de la lista resultado
+        # se agrega el resultado de esta sumatoria al final de la lista resultado
         resultado.append(suma)
         print(f"La suma de los digitos del DNI {digitos_dni} es {suma}")
 
@@ -199,10 +191,10 @@ numeros_unicos = generar_conjunto_digitos_unicos(dnis)
 ejecutar_operaciones_sobre_conjuntos(numeros_unicos)
 
 # Conteo de frecuencia de cada dígito en cada DNI usando estructuras repetitivas
-contar_frecuencia_digitos(dnis_completos)
+contar_frecuencia_digitos(dnis)
 
 # Suma total de dígitos de cada DNI
-sumar_total_digitos(numeros_unicos)
+sumar_total_digitos(dnis)
 
 # Expresión lógica 1:
 # Si algún conjunto tiene 7 o más dígitos únicos y al menos otro tiene solo 4 o menos, entonces se considera que el 
