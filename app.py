b6579a3dbc5a4b32cd99ac0aa8f6114457a63391
LucasJC <lucas.couchot@gmail.com>
2025-06-02 19:15:23 -0500

    Formateo y mensajes para indicar cada punto resuelto

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+import datetime
+
 def ingresar_dnis():
+    print("\n[Punto A.1 - Ingreso de los DNIs]")
     ingreso = input("Ingresar DNIs separados por coma o bien presionar ENTER para usar los DNIs por defecto: ")
     if ingreso.strip() == "":
         print("Usando DNIs por defecto")
         return [ "39057467", "37660974", "36899913", "36493716", "34330039" ]
+        
     else:
         return [ dni.strip() for dni in ingreso.split(",") ]
 
 def generar_conjunto_digitos_unicos(dnis):
-    #Mati
+    print("\n[Punto A.2 - Generación automática de conjuntos de dígitos únicos]")
     conjuntos = []
     for dni in dnis:
         conjunto = []
@@ -17,10 +21,11 @@ def generar_conjunto_digitos_unicos(dnis):
                 conjunto.append(numero)
         conjunto.sort()
         conjuntos.append(conjunto)
+    print(f"Conjuntos de dígitos únicos generados:\n{conjuntos}")
     return conjuntos
 
 def ejecutar_operaciones_sobre_conjuntos(numeros_unicos):
-    print("\nOperaciones sobre conjuntos:")
+    print("\n[Punto A.3 - Cálculo y visualización de: unión, intersección, diferencias y diferencia simétrica]")
     procesar_operaciones_conjuntos("Unión", union_conjuntos, numeros_unicos)
     procesar_operaciones_conjuntos("Intersección", interseccion_conjuntos, numeros_unicos)
     procesar_operaciones_conjuntos("Diferencia", diferencia_conjuntos, numeros_unicos, False)
@@ -74,7 +79,7 @@ def procesar_operaciones_conjuntos(titulo, funcion, conjuntos, pares_unicos = Tr
 
 # Función para contar la frecuencia de cada digito en cada DNI
 def contar_frecuencia_digitos(dnis_completos):
-    print("Frecuencia de dígitos:")
+    print("\n[Punto A.5 - Conteo de frecuencia de cada dígito en cada DNI]")
     # for que recorre cada dni de la lista
     for dni in dnis_completos:
         # se inicializa diccionario para almacenar la frecuencia por cada digito del 0 al 9
@@ -93,6 +98,7 @@ def contar_frecuencia_digitos(dnis_completos):
             print(f"Dígito {digito}: {cantidad} veces")
 
 def sumar_total_digitos(dnis_completos):
+    print("\n[Punto A.6 - Suma total de los dígitos de cada DNI]")
     # se define la lista vacia resultado para contener la suma de los digitos de cada dni
     resultado = []
     # for para recorrer cada sublista en la lista anidada. digitos_dni es la variable iterativa
@@ -108,7 +114,7 @@ def sumar_total_digitos(dnis_completos):
 
 
 def expresion_1(numeros_unicos):
-    # Nico D
+    print("\n[Punto A.7 - Evaluación de condiciones lógicas: Expresión 1]")
     hay_grande = any(len(conjunto) >= 7 for conjunto in numeros_unicos)
     hay_chico = any(len(conjunto) <= 4 for conjunto in numeros_unicos)
 
@@ -119,7 +125,7 @@ def expresion_1(numeros_unicos):
 
 
 def expresion_2(numeros_unicos):
-    # Nico D
+    print("\n[Punto A.7 - Evaluación de condiciones lógicas: Expresión 2]")
     pares = sum(1 for conjunto in numeros_unicos if len(conjunto) % 2 == 0)
     impares = len(numeros_unicos) - pares
 
@@ -132,6 +138,7 @@ def expresion_2(numeros_unicos):
 
 
 def ingresar_años_nacimiento():
+    print("\n[Punto B.1 - Ingreso de los años de nacimiento (Si dos o más integrantes del grupo tienen el mismo año, ingresar algún dato ficticio, según el caso)]")
     años = []
     cantidad = int(input("¿Cuántos años de nacimiento vas a ingresar? "))
     for i in range(cantidad):
@@ -140,7 +147,7 @@ def ingresar_años_nacimiento():
     return años
 
 def contar_nacidos_en_años_pares_e_impares(años_nacimiento):
-   # Mati
+    print("\n[Punto B.2 - Contar cuántos nacieron en años pares e impares utilizando estructuras repetitivas]")
     pares = 0
     impares = 0
 
@@ -149,87 +156,105 @@ def contar_nacidos_en_años_pares_e_impares(años_nacimiento):
             pares += 1
         else:
             impares += 1
-
+    
     print("Cantidad de nacidos en años pares:", pares)
     print("Cantidad de nacidos en años impares:", impares)
 
 def calcular_grupo_z(años_nacimiento):
-    # Macaris
+    print("\n[Punto B.3 - Si todos nacieron después del 2000, mostrar 'Grupo Z']")
+    # TODO implementar
     pass
 
 def es_año_bisiesto(año):
     return año % 4 == 0 and (año % 100 != 0 or año % 400 == 0)
 
 def determinar_años_bisiestos(años_nacimiento):
+    print("\n[Punto B.4 - Si alguno nació en año bisiesto, mostrar 'Tenemos un año especial']")
     for año in años_nacimiento:
         if es_año_bisiesto(año):
             print(f"Tenemos un año especial ({año})")
             return
 
 def calcular_producto_cartesiano(años_nacimiento):
-    # Mati
-   import datetime #Deberia ir al principio de la clase.
+    print("\n[Punto B.5 - Calcular el producto cartesiano entre el conjunto de años y el conjunto de edades actuales]")
 
-   año_actual = datetime.datetime.now().year
-   edades = []
+    año_actual = datetime.datetime.now().year
+    edades = []
 
     # Se calculan edades actuales a partir del año de nacimiento
-   for año in años_nacimiento:
-        edad = año_actual - año
-        edades.append(edad)
+    for año in años_nacimiento:
+            edad = año_actual - año
+            edades.append(edad)
 
     # Producto cartesiano entre años y edades
-   print("Producto cartesiano entre años de nacimiento y edades actuales:")
-   for año in años_nacimiento:
-        for edad in edades:
-            print((año, edad))
+    print("Producto cartesiano entre años de nacimiento y edades actuales:")
+    for año in años_nacimiento:
+            for edad in edades:
+                print((año, edad))
 
-print("Inicio de ejecución de parte A")
+def enter_para_continuar():
+    input("\nPresione ENTER para continuar...")
+
+
+print("[Inicio de ejecución de parte A]")
 
 # Ingreso de DNIs por teclado, por defecto tenemos los del grupo:
 # [ "39057467", "37660974", "36899913", "36493716", "34330039" ]
 dnis = ingresar_dnis()
+enter_para_continuar()
 
 # Generación de conjuntos de dígitos únicos, cada uno ordenado de menor a mayor. 
 # Ejemplo de salida: [[0, 3, 4, 5, 6, 7, 9], [0, 3, 4, 6, 7, 9], [1, 3, 6, 8, 9], [1, 3, 4, 6, 7, 9], [0, 3, 4, 9]]
 # Para entrada: [ "39057467", "37660974", "36899913", "36493716", "34330039" ]
 numeros_unicos = generar_conjunto_digitos_unicos(dnis)
+enter_para_continuar()
 
 # Cálculo y visualización de unión, intersección, diferencias y diferencias simétricas
 # Imprimirá por pantalla los resultados. Ejemplo: "A U B = {0, 3, 4, 5, 6, 7, 9}"
 ejecutar_operaciones_sobre_conjuntos(numeros_unicos)
+enter_para_continuar()
 
 # Conteo de frecuencia de cada dígito en cada DNI usando estructuras repetitivas
 contar_frecuencia_digitos(dnis)
+enter_para_continuar()
 
 # Suma total de dígitos de cada DNI
 sumar_total_digitos(dnis)
+enter_para_continuar()
 
 # Expresión lógica 1:
 # Si algún conjunto tiene 7 o más dígitos únicos y al menos otro tiene solo 4 o menos, entonces se considera que el 
-# grupo es “súper diverso”. En caso de que no se cumplan ambas condiciones a la vez, el grupo es “poco diverso”
+# grupo es "súper diverso". En caso de que no se cumplan ambas condiciones a la vez, el grupo es "poco diverso"
 expresion_1(numeros_unicos)
+enter_para_continuar()
 
 # Expresión lógica 2:
 # Si hay más conjuntos con cantidad par de elementos que con cantidad impar, entonces el grupo es "grupo par". En 
 # cambio, si hay más conjuntos con cantidad impar de elementos que con cantidad par, entonces se etiqueta como 
-# "grupo impar". Finalmente, en caso de empate, etiquetamos al grupo como “grupo de paridad neutra”
+# "grupo impar". Finalmente, en caso de empate, etiquetamos al grupo como "grupo de paridad neutra"
 expresion_2(numeros_unicos)
+enter_para_continuar()
 
 
-print("Inicio de ejecución de parte B")
+print("\n\n[Inicio de ejecución de parte B]")
 
 # Ingreso de años de nacimiento
 años_nacimiento = ingresar_años_nacimiento()
+enter_para_continuar()
 
 # Contar nacidos en años pares e impares
 contar_nacidos_en_años_pares_e_impares(años_nacimiento)
+enter_para_continuar()
 
 # Calcular si es un grupo Z(todos nacieron después del 2000). En caso afirmativo, mostrar mensaje "Grupo Z"
 calcular_grupo_z(años_nacimiento)
+enter_para_continuar()
 
 # Determinar qué años son bisiestos. Si alguno nació en un año bisiesto, debe mostrar mensaje "Tenemos un año especial"
 determinar_años_bisiestos(años_nacimiento)
+enter_para_continuar()
 
 # Calcular el producto cartesiano entre el conjunto de años y el conjunto de edades actuales
 calcular_producto_cartesiano(años_nacimiento)
+
+print("\n[Fin de ejecución]")
